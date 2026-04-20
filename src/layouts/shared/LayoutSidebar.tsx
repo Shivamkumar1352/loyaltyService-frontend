92b2c0112bfb3017c13fdc67b0af44330557f3a3
Shivamkumar1352 <shivamkumar1352002@gmail.com>
2026-04-20 19:15:05 +0530

    update

--- src/layouts/shared/LayoutSidebar.tsx
+++ src/layouts/shared/LayoutSidebar.tsx
@@ -13,9 +13,9 @@ export function LayoutSidebar({ sideOpen, onClose, header, nav, footer }: Layout
   return (
     <aside
       className={`
-        fixed inset-y-0 left-0 z-40 flex w-[85vw] min-w-[85vw] max-w-72 flex-shrink-0 flex-col
+        fixed inset-y-0 left-0 z-40 flex w-[85vw] min-w-[85vw] max-w-72 flex-shrink-0 flex-col overflow-y-auto
         transition-transform duration-300 ease-in-out
-        lg:static lg:z-auto lg:w-64 lg:min-w-64 lg:max-w-64 lg:translate-x-0
+        lg:sticky lg:top-0 lg:z-auto lg:h-screen lg:w-64 lg:min-w-64 lg:max-w-64 lg:basis-64 lg:translate-x-0
         ${sideOpen ? 'translate-x-0' : '-translate-x-full'}
       `}
       style={{ background: 'var(--bg-secondary)', borderRight: '1px solid var(--border)' }}
